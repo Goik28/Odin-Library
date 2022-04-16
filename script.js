c298f7f356f383878cd43440462cd4ee7a992cd5
Samuel <sbabinski@outlook.com>
2022-04-16 14:07:17 -0300

    Add books complete, remove books not yet
    Organize the code a little.
    Start to work in the remove books logic.
    Have to fix the event handler not adding.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let newBook_Toggle = false;
 
 const newBook_Add = document.getElementById("newBook_Add");
 
+const totalBooks = document.getElementsByClassName("total_Books")[0];
+
 function book(title, author, book_Pages, read) {
     this.title = title;
     this.author = author;
@@ -28,34 +30,56 @@ function createBook() {
     let aBook = new book(document.getElementById("book_Title").value,
         document.getElementById("book_Author").value,
         document.getElementById("book_Pages").value,
-        document.getElementById("book_Read").value
+        document.getElementById("book_Read").checked
     );
     return aBook;
+}
+
+function cleanBookContainer() {
+    let container = document.getElementsByClassName("book_Container")[0];
+    const model = document.getElementById("card_Model").cloneNode(true);
+    container.innerHTML = "";
+    container.appendChild(model);
+}
+
+function createBookCards() {
+    library.forEach((element, index) => {
+        document.getElementById("index_Card").textContent = "" + (index + 1);
+        document.getElementById("title_Card").textContent = element.title;
+        document.getElementById("author_Card").textContent = element.author;
+        document.getElementById("total_Pages_Card").textContent = element.book_Pages;
+        document.getElementById("read_Card").checked = element.read;
+        let the_Book = document.getElementsByClassName("book_Card")[0].cloneNode(true);
+        the_Book.id = "card_"+index;
+        the_Book.lastChild.addEventListener("click", findBookToBeRemoved);
+        document.getElementsByClassName("book_Container")[0].appendChild(the_Book);
+    });
+}
+
+function updateTotalBooks() {
+    totalBooks.textContent = " " + library.length;
 }
 
 function addBookToLibrary() {
     let aBook = createBook();
     library.push(aBook);
-    createBookCard();
+    cleanBookContainer();
+    createBookCards();
+    updateTotalBooks();
 }
-
-
 
 newBook_Add.addEventListener("click", addBookToLibrary);
 
-function cleanBookContainer(){
-    let container = document.getElementsByClassName("book_Container")[0];
-    container.innerHTML = document.getElementsByClassName("book_Card")[0];
+const removeButton = document.getElementById("book_Remove");
+
+function findBookToBeRemoved (event){
+    const bookToBeRemoved = event.target.parentNode.id;
+    removeBookFromLibrary(bookToBeRemoved.substr(5));
 }
 
-function createBookCard() {
-    library.forEach( (element, index) => {
-        document.getElementById("index_Card").textContent = ""+index;
-        document.getElementById("title_Card").textContent = element.title;
-        document.getElementById("author_Card").textContent = element.author;
-        document.getElementById("total_Pages_Card").textContent = element.book_Pages;
-        document.getElementById("read_Card").textContent = element.read;
-        let the_Book = document.getElementsByClassName("book_Card")[0].cloneNode(true);
-        document.getElementsByClassName("book_Container")[0].appendChild(the_Book);
-    });
+function removeBookFromLibrary (index){
+    library.splice(index,1);
+    cleanBookContainer();
+    createBookCards();
+    updateTotalBooks();
 }
